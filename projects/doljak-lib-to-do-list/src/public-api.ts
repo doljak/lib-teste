514579c805a7332723715f19bdd77d0aa415dacb
Jefferson de Melo Doljak <doljak@MacBook-Pro-de-Jefferson.local>
2025-09-25 16:57:16 -0300

    feat: public api exports pages

--- projects/doljak-lib-to-do-list/src/public-api.ts
+++ projects/doljak-lib-to-do-list/src/public-api.ts
@@ -5,3 +5,7 @@
 export * from './lib/doljak-lib-to-do-list.service';
 export * from './lib/doljak-lib-to-do-list.component';
 export * from './lib/doljak-lib-to-do-list.routes';
+
+export * from './lib/pages/cms/cms.component';
+export * from './lib/pages/todo-list/todo-list.component';
+export * from './lib/pages/login/login.component';
